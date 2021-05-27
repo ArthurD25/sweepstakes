38c5075687270c704215b5edb8f81b3429ed0c1b
ArthurD25 <arthuro.oneal@gmail.com>
2021-05-27 10:30:22 -0400

    created a sweepstakes class with methods to create a dictionary for the contestants and select a randomwinner.

--- sweepstakes.py
+++ sweepstakes.py
@@ -1,2 +1,18 @@
+from contestant import Contestant
+import random
 class Sweepstakes:
-    
+    def __init__(self,name):
+        self.sweepstakes_name = name
+        self.contestant = {}
+
+    def register_contestant(self,contestant):
+        self.contestant[contestant.registration_number] = contestant
+
+    def pick_winner(self):
+        winner = random.choice(self.contestant)
+        return winner
+
+
+    def print_contestant_info(self,contestant):
+        print('l')
+
